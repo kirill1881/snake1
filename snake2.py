3b4b599229ceda5b359bb4bceb19c4180d0f95cb
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2023-03-03 11:49:55 +0300

    Create snake2.py

---
+++ snake2.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+import time
+import random
+
+difficulty = 25
+
+frame_size_x = 720
+frame_size_y = 480
+
+check_errors = pygame.init()
+
+pygame.display.set_caption('Snake')
+game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
+
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+fps_controller = pygame.time.Clock()
+
+snake_pos = [100, 50]
+snake_body = [[100, 50], [90, 50], [80, 50]]
+
+food_pos = [random.randrange(1, (frame_size_x//10) * 10),  random.randrange(1, (frame_size_x//10) * 10)]
+food_spawn = True
+
+score = 0
+
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('GAME OVER', True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
+
+
+
+
+
+
+
+
+
+
